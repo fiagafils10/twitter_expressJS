df846d99a12e0165d75048559b3fd9e5626a46ae
Fiaga Fils <fiagafils@gmail.com>
2023-01-05 17:02:51 +0100

    tweet-form created

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://joker:<passwordhere>@dymalearn.caquiko.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://joker:<password here>@dymalearn.caquiko.mongodb.net/?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   ).then(console.log("connection ok")).catch((err) => console.log(err));
     
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 
 
+router.get('/tweet/new', (req, res)=> {
+    res.render('../views/tweets/tweet-form.pug')
+})
+
 router.get('/', (req,res)=> {
     res.render('../views/tweets/tweet-list.pug') //venant de pug, enfaite cette route va nous diriger vers la page d'acceuil
 })
